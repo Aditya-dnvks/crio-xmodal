03da7ab143c2c635101aa5050e3a60f774726220
Aditya-dnvks <aditya.dnvks@gmail.com>
2024-12-10 00:45:29 +0530

    added modal to app.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import XModal from "./modal";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <XModal />
+    </div>
+  );
 }
 
 export default App;
